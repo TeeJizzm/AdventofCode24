141c775fd73522518cd7444dc0e9d477faf712cc
TeeJizzm <teejizzm@gmail.com>
2024-12-01 13:39:10 +0000

    Day 01 complete in Go

--- day01/day01.go
+++ day01/day01.go
@@ -17,50 +17,102 @@ import (
 
 	// String manipulation
 	"strings"
-	//"strconv"
-	// Other required imports
+	// Other required import
+	"slices"
 )
 
-/* -----------------------------------
-// Functions */
+// -----------------------------------
+// Functions //
 
-func part_one(lines []string) int {
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func countOcc(slice []int, target int) int {
+	count := 0
+	for _, value := range slice {
+		if value == target {
+			count++
+		}
+	}
+	return count
+}
+
+func part_one(col_a []int, col_b []int) int {
 	var part1 int = 0
+
+	//fmt.Println(col_a)
+	//fmt.Println(col_b)
+
+	for i, a := range col_a {
+		diff := a - col_b[i]
+
+		part1 += max(diff, -diff)
+	}
 
 	return part1
 }
 
-func part_two(lines []string) int {
+func part_two(col_a []int, col_b []int) int {
 	var part2 int = 0
+
+	set_a := make([]int, len(col_a))
+	copy(set_a, col_a)
+
+	slices.Compact(set_a)
+
+	for _, num := range set_a {
+		cnt_a := countOcc(col_a, num)
+		cnt_b := countOcc(col_b, num)
+
+		val := cnt_a * cnt_b * num
+
+		part2 += val
+	}
 
 	return part2
 }
 
 func main() {
-	fmt.Println("Day 01 - *NAME*")
+	fmt.Println("Day 01 - Historian Hysteria")
 
 	// ------------ File -------------
 
-	//* - File switch
-	file, err := os.ReadFile("ex.txt")
-	//*/ //
-	file, err := os.ReadFile("in.txt")
+	/* <- File switch
+	file, err := os.ReadFile("day01/inc/ex.txt")
+	/*/ //
+	file, err := os.ReadFile("day01/inc/in.txt")
 	// --- End --- */
 
-	if err != nil {
-		fmt.Println(err)
-	}
+	check(err)
 
 	// Added fix for Windows newline
 	text := strings.ReplaceAll(string(file), "\r\n", "\n")
-	fmt.Print(text)
+	//fmt.Println(text)
 
 	// ----------- Setup -------------
 
 	lines := strings.Split(text, "\n")
 
-	part1 := part_one(lines)
-	part2 := part_two(lines)
+	col_a := make([]int, len(lines))
+	col_b := make([]int, len(lines))
+
+	for i, line := range lines {
+		var a, b int
+		_, err = fmt.Sscan(line, &a, &b)
+		check(err)
+
+		col_a[i] = a
+		col_b[i] = b
+	}
+
+	slices.Sort(col_a)
+	slices.Sort(col_b)
+
+	part1 := part_one(col_a, col_b)
+	part2 := part_two(col_a, col_b)
 
 	// ----------- Output ------------
 
