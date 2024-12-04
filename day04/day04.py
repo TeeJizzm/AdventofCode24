7afd95d9f869f4120abf140ba937727149d82048
Teejizzm <teejizzm@gmail.com>
2024-12-04 21:55:22 +0000

    Day 04 - Complete in Python

--- day04/day04.py
+++ day04/day04.py
@@ -11,22 +11,48 @@
 import os
 
 import tools.texttolists as tl
+import tools.grids as grids
 
 ############################
 # Variables
 
-
+WORD = "XMAS"
 
 ############################
 # Functions
 
+def findXMAS(grid):
+
+    locs = grids.findLocs(grid, "A")
+
+    count = 0
+
+    dirs = [(1,1),(1,-1)] # Diagonals
+
+    for x, y in locs:
+        mas = 0
+        for dx, dy in dirs:
+            #print("".join([grid[x+(dx*i)][y+(dy*i)] for i in range(-1,2)]))
+            if "".join([grid[x+(dx*i)][y+(dy*i)] for i in range(-1,2)]) in ("MAS", "SAM"):
+                mas += 1
+
+        if mas == 2:
+            count += 1
+
+    return count
+
+
 def day04(text):
-    print("Day 04 - *NAME*")
-    
-    part1, part2 = text, ''
-    
-    
-    
+    print("Day 04 - Ceres Search")
+
+    padding = len(WORD) -1
+
+    part1, part2 = 0, 0
+    grid = grids.padArray([list(line) for line in tl.toLines(text)], padding)
+
+    part1 = grids.findWord(grid, WORD)
+    part2 = findXMAS(grid)
+
     return part1, part2
 
 ############################
@@ -37,8 +63,9 @@ if __name__ == "__main__":
     
     # Change file
     #######
+    file = "a.txt"
     file = "ex.txt"
-    #file = "in.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
