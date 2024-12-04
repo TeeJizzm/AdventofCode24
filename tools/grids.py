596ee39b11789ce3bd38bdf8b229d4c65edae8fb
Teejizzm <teejizzm@gmail.com>
2024-12-04 21:55:45 +0000

    Added grids.py to tools folder

---
+++ tools/grids.py
@@ -0,0 +1,53 @@
+# Reads a text input and returns separated values
+# Returns a list of lists
+
+###########################
+
+def padArray(grid, padding=2):
+
+    cols = padding + len(grid[0]) + padding
+
+    for row in grid:
+        for _ in range(padding):
+            row.insert(0, ".")
+            row.append(".")
+
+    for _ in range(padding):
+        grid.insert(0, ["."]*cols)
+        grid.append(["."]*cols)
+
+    return grid
+
+def findLocs(grid, target):
+
+    locations = []
+
+    for x, row in enumerate(grid):
+        for y, v in enumerate(row):
+            if v == target:
+                locations.append((x,y))
+                #print(target, end="")
+            else:
+                #print(v, end="")
+                #print(".", end="")
+                continue
+        #print()
+
+    return locations
+
+def findWord(grid, word):
+
+    locs = findLocs(grid, word[0])
+
+    count = 0
+
+    dirs = [(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0),(-1,1)] # Clockwise directions
+
+    for x, y in locs:
+        for dx, dy in dirs:
+            if "".join([grid[x+(dx*i)][y+(dy*i)] for i in range(len(word))]) == word:
+                count += 1
+
+    return count
+
+########## EOF ############
