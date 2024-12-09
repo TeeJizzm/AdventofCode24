9c45bf6769f7aa20de8f9ba5e22d2c42a9918bde
Teejizzm <teejizzm@gmail.com>
2024-12-09 12:04:40 +0000

    Day 09 - Complete in Python

--- day09/day09.py
+++ day09/day09.py
@@ -20,13 +20,87 @@ import tools.texttolists as tl
 ############################
 # Functions
 
+def constructData(entries):
+    data = []
+    
+    x = 0
+
+    for idx, num in enumerate(entries):
+
+        if idx % 2 == 0:
+            data += [x]* int(num)
+            x += 1
+            #print(data)
+        else:
+            data += ["."]* int(num)
+            #print(data)
+
+    return data
+
+def reorgData(data):
+
+    for idx in range(len(data) - 1, -1, -1):
+        if data[idx] != ".":
+            jdx = data.index(".")
+            if jdx > idx:
+                break
+            data[jdx] = data[idx]
+            data[idx] = "."
+
+        #print(data)
+
+    return data
+
+def reorgFiles(data, entries):
+
+    lengths = [num for idx, num in enumerate(entries) if idx % 2 == 0]
+
+    v = len(lengths)-1
+    width = int(lengths[v])
+
+    for idx in range(len(data) - 1, -1, -1):
+        #print(val)   
+        if data[idx] not in [".", "*"]:
+            if int(data[idx]) == v:
+                
+                #print(idx, v)
+                for jdx in range(len(data)-1):
+                    if jdx > idx: break
+                    if data[jdx:jdx+width] == ["."] * width:
+                        data[jdx:jdx+width] = [v]*width
+                        data[idx-width+1:idx+1] = ["."] * width
+                        #print(jdx, ["."]*int(lengths[v]))
+                        break
+
+                #print(data[idx], lengths[v])
+                v -= 1 
+                width = int(lengths[v])   
+        #print(data)
+
+    return data
+
+
+def getCheckSum(data):
+
+    return sum([idx * int(val) for idx, val in enumerate(data) if val != "."])
+
 def day09(text):
-    print("Day 09 - *NAME*")
+    print("Day 09 - Disk Fragmenter")
     
-    part1, part2 = text, ''
+    part1, part2 = 0, 0
     
-    
-    
+    entries = list(text)
+    #print(entries)
+    dataLine = constructData(entries)
+
+    #print(dataLine)
+
+    defragmented = reorgData(dataLine[:])
+    sortedFiles = reorgFiles(dataLine[:], entries)
+
+    part1 = getCheckSum(defragmented)
+    part2 = getCheckSum(sortedFiles)
+
     return part1, part2
 
 ############################
@@ -38,7 +112,7 @@ if __name__ == "__main__":
     # Change file
     #######
     file = "ex.txt"
-    #file = "in.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
