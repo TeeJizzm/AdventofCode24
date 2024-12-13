cd8b71e2703b44197b58d8e7214f67225f067a71
Teejizzm <teejizzm@gmail.com>
2024-12-13 12:00:08 +0000

    Day 13 - Complete in Python

--- day13/day13.py
+++ day13/day13.py
@@ -12,6 +12,9 @@ import os
 
 import tools.texttolists as tl
 
+import numpy as np
+import re
+
 ############################
 # Variables
 
@@ -20,13 +23,68 @@ import tools.texttolists as tl
 ############################
 # Functions
 
+def cramer(machine, offset = 0):
+
+    A, B = 0, 0
+
+    ax, ay, bx, by, px, py = re.findall(r"\d+", machine)
+
+    px = int(px) + offset
+    py = int(py) + offset
+
+    print(f"Testing: {ax}x + {ay}y = {px}; {bx}x + {by}y = {py}")
+
+    d = (int(ax)*int(by))-(int(ay)*int(bx))
+    d_a = (px*int(by))-(py*int(bx))
+    d_b = (int(ax)*py)-(int(ay)*px)
+
+    if d_a % d == 0 and d_b % d == 0:
+        A = int(d_a / d)
+        B = int(d_b / d)
+    
+    return A, B
+
+def findEq(machine):
+
+    ax, ay, bx, by, px, py = re.findall(r"\d+", machine)
+
+    print(f"Testing: {ax}x + {ay}y = {px}; {bx}x + {by}y = {py}")
+
+    ab = np.array([[int(ax), int(bx)], [int(ay), int(by)]])
+    xy = np.array([int(px), int(py)])
+
+    a, b = np.linalg.solve(ab, xy)
+
+    print("a:", a, "b:", b)
+
+    if 0 < a > 100 or 0 < b > 100:
+        a = 0
+        b = 0
+
+    elif abs(a - np.round(a) > 1e-3) or abs(b - np.round(b) > 1e-3):
+        a = 0
+        b = 0
+
+    print("a-", np.round(a), "b-", np.round(b))
+
+    return int(np.round(a)), int(np.round((b)))
+
+
 def day13(text):
-    print("Day 13 - *NAME*")
+    print("Day 13 - Claw Contraption")
     
-    part1, part2 = text, ''
+    part1, part2 = 0, 0
     
-    
-    
+    machines = tl.toLines(text, "\n\n")
+
+
+    for machine in machines:
+        #a, b = findEq(machine)
+        a, b = cramer(machine)
+        part1 += ((3*a) + b)
+        a, b = cramer(machine, 10000000000000)
+        part2 += ((3*a) + b)
+
     return part1, part2
 
 ############################
@@ -38,7 +96,7 @@ if __name__ == "__main__":
     # Change file
     #######
     file = "ex.txt"
-    #file = "in.txt"
+    file = "in.txt"
     #######
     
     # Get absolute filepath of file
